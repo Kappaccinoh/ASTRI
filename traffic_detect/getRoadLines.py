9c2bdfa85bd61399303e6706fe3837aaeda3d80c
Kappaccinoh <lim.jia28@gmail.com>
2024-08-05 11:56:36 +0800

    a

--- traffic_detect/getRoadLines.py
+++ traffic_detect/getRoadLines.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 def rgb_to_hsv(r, g, b):
     color = np.uint8([[[b, g, r]]])
@@ -41,42 +42,100 @@ image_path = "Camera_Images_Label2/AID03101_20240801_111812.JPG"
 green_line_length = measure_line_length(image_path, lower_green, upper_green)
 
 print(f"Green line length: {green_line_length} pixels")
+
+
+import cv2
+
 def measure_red_line_lengths(image_path, lower_color, upper_color):
     image = cv2.imread(image_path)
     
     if image is None:
         print(f"Failed to load image: {image_path}")
-        return []
+        return [], []
     
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_color, upper_color)
-    image_url = url.replace("colour", "red")
     
-    cv2.imwrite(image_url, mask)
+    cv2.imwrite("masked_image.png", mask)
 
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     lengths = [cv2.arcLength(contour, True) for contour in contours]
-    
-    return lengths
+    start_points = [contour[0][0] for contour in contours]
+
+    return lengths, start_points
+
 
 red_line_lengths = measure_red_line_lengths(image_path, lower_red, upper_red)
-
-print(f"Red line lengths: {red_line_lengths} pixels")
+print(f"Start of contour XY Coordinates with TOP LEFT as (0,0): {red_line_lengths[1]}")
+print(f"Red line lengths: {red_line_lengths[0]} pixels")
 
 
 # Assume 4 metre lines
 
-def getRoadLength(target_line, ref_list):
-    grad = 0
-    for i in range(len(ref_list)-1):
-        temp = ref_list[i] - ref_list[i+1]
-        grad += temp / 2
-    grad = grad / (len(ref_list)-1)
-    print(grad)
-    total_length = target_line / grad
-    return total_length
+# def getRoadLength(target_line, ref_list):
+#     grad = 0
+#     ref_list = [l / 4 for l in ref_list]
+#     for i in range(len(ref_list)):
+#         temp = ref_list[i]
+#         print(temp)
+#         grad += temp / 2
+#     grad = grad / (len(ref_list)-1)
+#     print(grad)
+#     total_length = target_line / grad
+#     return total_length
 
 
-road_length = getRoadLength(green_line_length, red_line_lengths)
-print(f"Road is: {road_length}m")
+# road_length = getRoadLength(green_line_length, red_line_lengths[0])
+# print(f"Road is: {road_length}m")
+
+
+def linear_extrapolation(Y_coords, Y_grads, Y_max):
+    m = 0
+    for i in range(len(Y_coords)-1):
+        Y1, m1 = Y_coords[i], Y_grads[i]
+        Y2, m2 = Y_coords[i+1], Y_grads[i+1]
+        m += (m1-m2) / (Y1 - Y2)
+        print(m)
+    m = m / (len(Y_coords) -1)    
+    grad_Y_max = Y_grads[0] + m * (Y_max - Y_coords[0])
+    
+    return grad_Y_max
+
+
+# def getRoadLength(target_line, ref_list):
+#     grad = 0
+#     ref_list = [l / 4 for l in ref_list]
+#     for i in range(len(ref_list)):
+#         temp = ref_list[i]
+#         print(temp)
+#         grad += temp / 2
+#     grad = grad / (len(ref_list)-1)
+#     print(grad)
+#     total_length = target_line / grad
+    
+#     return length
+
+
+
+Y_max = 240  
+
+Y_grads = [l/4 for l in red_line_lengths[0]]
+print(Y_grads)
+Y_coords = [y for _, y in red_line_lengths[1]]
+print(Y_coords)
+grad_Y_max = linear_extrapolation(Y_coords, Y_grads, Y_max)
+print(f"Gradient at Y_max: {grad_Y_max:.2f}")
+
+def length_from_grad(grad, Y_max):
+    res = 0
+    dec = grad/Y_max
+    pixel_metre_ratio = grad
+    for _ in range(Y_max):
+        res += 1/pixel_metre_ratio
+        pixel_metre_ratio -= dec
+    return res
+
+road_len = length_from_grad(grad_Y_max, Y_max)
+print(f"Road has length: {road_len:.2f}m")
+
